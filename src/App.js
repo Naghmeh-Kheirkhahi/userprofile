55c015e7b2fc9b4bea439a3e463efc4b84a7c44f
naghmeh <melodycharitable4774@gmail.com>
2024-10-10 20:24:50 +0200

    make a child and parent for the shopping API

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function App() {
     
     {/* <Button/>
 
+    <Form/>
 
-    <Form/> */}
+    <Test/> */}
 
-    <Test/>
+    
     
     </>
   );
--- src/Components/Test.js
+++ src/Components/Test.js
@@ -9,33 +9,33 @@ function Test() {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(null); // the default of the error in useState is null because there is no error by default.
 
-    useEffect(() =>{
+    useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(res =>{
-            console.log(res);
-            setData(res.data)
-            setLoading(false);
-        })
-        .catch(error=>{
+            .then(res => {
+                console.log(res);
+                setData(res.data)
+                setLoading(false);
+            })
+            .catch(error => { // we use it to show the errors found.
 
-         setError(error.message)
-        console.log(error);
-        setLoading(true);
-        })
-    },[])
+                setError(error.message)
+                console.log(error);
+                setLoading(true); // when we face any error , the web page must be loaded.
+            })
+    }, [])
 
 
 
-if(loading){
+    if (loading) { // when loading is true, the web page will be loaded and this bellow message will be shown.
 
-    return <p>Loading...</p>
-}
+        return <p>Loading...</p>
+    }
 
-if (error) {
-    return <p>Error is: {error}</p>
-}
+    if (error) { // when error is true, the web page will have error and this bellow message will be shown.
+        return <p>Error is: {error}</p>
+    }
 
 
 
@@ -43,7 +43,7 @@ if (error) {
 
 
         <>
-            {data.map(item=>(
+            {data.map(item => (
                 <div>
                     <div>{item.username}</div>
                     <div>{item.email}</div>
@@ -58,18 +58,12 @@ if (error) {
                     <div>{item.company.name}</div>
                     <div>{item.company.catchPhrase}</div>
                     <div>{item.company.bs}</div>  <div>{item.name}</div>
-                    <Button 
 
-                    name={item.name}
-                    email={item.email}
-                    phone={item.phone}
+                    <Button/>
 
-
-/>
-                  
                 </div>
             ))}
-        
+
         </>
     )
 }
