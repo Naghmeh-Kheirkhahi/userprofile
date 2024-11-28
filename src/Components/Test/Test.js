1a197ce265054ca6267486c55d4e1328dc3449d7
naghmeh <melodycharitable4774@gmail.com>
2024-11-28 12:41:27 +0100

    practiced useContext and createContext

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -3,20 +3,38 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../../Context/ThemeContext";
 
 
+
+
+// function Test() { we can write it in this way as well.
+//     return (
+
+//         <>
+//             <div>
+//             </div>
+//         </>
+    
+//     )
+// }
+// export default Test;
+
+
+
 const Test = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     const myStyle = {
         backgroundColor: theme === 'light' ? 'white' : 'black',
         color: theme === 'light' ? 'black' : 'white',
-        padding: '1rem',
+        textAlign: 'center',
+        padding: '5rem',
     }
 
     return (
         <>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br />
             <div style={myStyle}>
                 <h1>{theme === "light" ? 'This is called Light Test.' : 'This is called Dark Test.'}</h1>
+                <br />
                 <button onClick={toggleTheme}>Change Theme</button>
             </div>
 
--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -2,14 +2,18 @@
 import React, {createContext, useState} from "react";
 
 
+
+
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({children}) => {
+
     const [theme, setTheme] = useState('light');
 
     const toggleTheme = () => {
         setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light');
     }
+
 
     return (
         <ThemeContext.Provider value={{theme, toggleTheme}}>
