a7f2f6f33f103701c88b7fe6f82be59c9d64d87e
naghmeh <melodycharitable4774@gmail.com>
2025-02-16 21:06:25 +0100

    editted it again

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -2,8 +2,6 @@
 import React, { createContext, useEffect, useState } from "react";
 
 
-
-
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
@@ -15,7 +13,6 @@ export const ThemeProvider = ({ children }) => {
     const toggleTheme = () => {
         setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
     }
-
     // children are used  to wrap the components that will be used in the app. It is used to connect the components to the ThemeContext.
     // we need to save the last state of theme in the local storage to keep the theme when the page is refreshed. We can use the localStorage.setItem method to save the theme in the local storage and the localStorage.getItem method to get the theme from the local storage.
 
@@ -32,10 +29,7 @@ export const ThemeProvider = ({ children }) => {
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
         </ThemeContext.Provider>
-
         // we can connect  theme and toggleTheme to the all pages and components included the whole app by using this code.
     )
 }
-
-
 // the difference between export and export default is that we can export multiple items from a file by using export and the imported items must use the exact names defined in the export, but we can export only one item by using export default and the name used for the imported item can be different from the export name when it is used in app.js.
