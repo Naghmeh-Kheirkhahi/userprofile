0d9c35f6fb42994499c7c98cb137586a088da99b
naghmeh <melodycharitable4774@gmail.com>
2025-07-13 19:12:22 +0200

    fixed a mistake in Home page

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -63,50 +63,6 @@ function Nav() {
                 </div>
             </div>
         </nav>
-
-
-        // <div className='mainMenu'>
-        //     <nav class="navbar navbar-expand-lg">
-        //         <div class="container-fluid">
-        //             <Link class="navbar-brand navLogo" to="/"><img src={logo} alt="Logo" /></Link>
-
-        //             <div class="collapse navbar-collapse" id="navbarNav">
-        //                 <ul class="navbar-nav">
-        //                     <li class="nav-item">
-        //                         <Link class="active navLink" aria-current="page" to="/">Home</Link>
-        //                     </li>
-        //                     <li class="nav-item">
-        //                         <Link class="navLink" to="/Contact">Contact Us</Link>
-        //                     </li>
-        //                     <li class="nav-item">
-        //                         <Link class="navLink" to="/About">About Us</Link>
-        //                     </li>
-        //                 </ul>
-        //             </div>
-
-        //             {username ? (
-        //                 <div className='navDropdown'>
-        //                     <Link className='navLogin' to='/UserPanel'><i class="fa fa-user"></i> {username}</Link>
-        //                     <ul>
-        //                         <li><Link to='/UserPanel/Orders'>Orders</Link></li>
-        //                         <li><Link to='/UserPanel/Address'>Address</Link></li>
-        //                         <li><Link to='/UserPanel/Payment'>Payment List</Link></li>
-        //                         <li><Link to='/UserPanel/Interests'>Interests</Link></li>
-        //                         <li><Link to='/UserPanel/PersonalInfo'>Personal Information</Link></li>
-        //                     </ul>
-        //                 </div>
-        //             ) : (
-        //                 <Link className='navLogin' to='/Login'><i class="fa fa-user"></i> Login</Link>
-        //             )}
-
-        //             <div className='navBtn'>
-        //                 <button id='navBtn' onClick={toggleTheme} className={theme === 'dark' ? 'dark-navBtn' : 'light-navBtn'}>
-        //                     {theme === 'dark' ? <i class='fas fa-moon'></i> : <i class='fas fa-sun'></i>}
-        //                 </button>
-        //             </div>
-        //         </div>
-        //     </nav>
-        // </div>
     )
 }
 
